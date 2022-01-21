1081f526af630c19d2eb8e9c14fe1a746f23f6d0
Antônio Cosmo <antoniocosmosilvaneto@gmail.com>
2022-01-21 10:39:27 -0300

    connection with api

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,35 @@
-function App() {
-  return <div>marvel</div>;
+import { useEffect, useState } from 'react';
+
+import { Api } from './services/Api';
+import { GlobalStyle } from './styles/GlobalStyle';
+
+interface ICharacter {
+  id: number;
+  name: string;
+  description: string;
+  thumbnail: {
+    path: string;
+    extension: string;
+  };
 }
 
-export default App;
+export function App() {
+  const [loadCharacters, setLoadCharacters] = useState<ICharacter[]>([]);
+  const limit = 8;
+
+  useEffect(() => {
+    async function loader() {
+      try {
+        const response = await Api.get('characters', { params: { limit } });
+        const { results } = response.data.data;
+        setLoadCharacters(results);
+      } catch (e) {
+        console.log(e);
+      }
+    }
+    loader();
+  }, []);
+  console.log(loadCharacters);
+
+  return <GlobalStyle />;
+}
---
+++ src/styles/GlobalStyle.tsx
@@ -0,0 +1,71 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyle = createGlobalStyle`
+
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  html {
+
+    @media (max-width: 1080px){
+      font-size: 93.75%;
+    }
+
+    @media (max-width: 720px){
+      font-size: 87.5%;
+    }
+  }
+
+  body, input, textarea, button {
+    font-family: 'Marvel', sans-serif;
+    font-weight: 400;
+  }
+
+  h1, h2, h3, h4, h5, h6, strong {
+    font-weight: 700;
+  }
+
+  button {
+    cursor: pointer;
+  }
+
+  .react-modal-content{
+    width: 700px;
+    background-color: #202020 ;
+    color: #ffffff;
+    @media (min-width: 880px){
+      width: 550px;
+    }
+    @media (min-width: 650px) and (max-width:879px){
+      width: 480px;
+    }
+    @media (min-width: 500px) and (max-width:649px){
+      max-width: 440px;
+    }
+
+    @media (min-width:350px) and (max-width:499px){
+      max-width: 300px;
+    }
+
+    @media (max-width:349px){
+      max-width: 250px;
+    }
+  }
+  .react-modal-overlay{
+    position: fixed;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    z-index: 999;
+    background: rgba(255, 255, 255, 0.75);
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+  }
+
+`;
